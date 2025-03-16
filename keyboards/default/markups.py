d5e48c8942692aa773c6d757f82518e8d07f6202
Van <vanmkrtchyan7.com>
2025-03-16 19:56:43 +0400

    Добавил новые файлы и обновил существующие в проекте aio_bot_prj

--- keyboards/default/markups.py
+++ keyboards/default/markups.py
@@ -3,6 +3,7 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup
 back_message = '👈 Назад'
 all_right_message = '✅ Все верно'
 cancel_message = '🚫 Отменить'
+confirm_message = '✅ Подтвердить заказ'
 
 def back_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
@@ -12,5 +13,18 @@ def back_markup():
 def check_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(back_message, all_right_message)
+
+    return markup
+
+def confirm_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(confirm_message)
+    markup.add(back_message)
+
+    return markup
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
 
     return markup
--- states/__init__.py
+++ states/__init__.py
@@ -1,5 +1,7 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from  .product_state import ProductState
-
+from .checkout_state import CheckoutState
+from .sos_state import SosState
+from .questions import AnswerState
 class CategoryState(StatesGroup):
     title = State()
